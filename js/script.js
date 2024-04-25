41e7253c29ed2b27f2afdf9d33d1b1c7dd5b6abe
kpao2020 <xoakuma@gmail.com>
2024-04-25 11:31:19 -0700

    finish footer cta

--- js/script.js
+++ js/script.js
@@ -1,37 +1,18 @@
-const c1 = document.getElementById('about');
-const c2 = document.getElementById('c2');
-const c3 = document.getElementById('c3');
-const b1 = document.getElementById('btn1');
-const b2 = document.getElementById('btn2');
-const b3 = document.getElementById('btn3');
+(() => {
+  'use strict'
 
-const bioOpenBtn = document.getElementById('bio-open-btn');
-const bioCloseBtn = document.getElementById('bio-close-btn');
-const bioText = document.getElementById('bio-text');
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation')
 
-b1.addEventListener("click", () => {
-    c1.classList.toggle("d-none");
-    c2.classList.toggle("d-none");
-});
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
 
-b2.addEventListener("click", () => {
-    c2.classList.toggle("d-none");
-    c3.classList.toggle("d-none");
-});
-
-b3.addEventListener("click", () => {
-    c3.classList.toggle("d-none");
-    c1.classList.toggle("d-none");
-});
-
-bioOpenBtn.addEventListener("click", () => {
-    bioText.classList.toggle("d-none");
-    bioOpenBtn.classList.toggle("d-none");
-    bioCloseBtn.classList.toggle("d-none");
-});
-
-bioCloseBtn.addEventListener("click", () => {
-    bioText.classList.toggle("d-none");
-    bioOpenBtn.classList.toggle("d-none");
-    bioCloseBtn.classList.toggle("d-none");
-});
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()
