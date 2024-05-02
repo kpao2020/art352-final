8ae8b82f2a1d626ca856a20b8addcaee721c891e
kpao2020 <xoakuma@gmail.com>
2024-05-01 23:47:08 -0700

    test google form script

--- js/script-test.js
+++ js/script-test.js
@@ -37,23 +37,39 @@ bioCloseBtn.addEventListener("click", () => {
 });
 
 (() => {
-  'use strict'
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        } 
+        else {
+          alert("Thank you for submitting your contact information successfully.");
+        }
+  
+        form.classList.add('was-validated')
+      }, false)
+    })
+  })()
 
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  const forms = document.querySelectorAll('.needs-validation')
-
-  // Loop over them and prevent submission
-  Array.from(forms).forEach(form => {
-    form.addEventListener('submit', event => {
-      if (!form.checkValidity()) {
-        event.preventDefault()
-        event.stopPropagation()
-      } 
-      else {
-        alert('Thank you for submitting your contact information successfully.');
-      }
-
-      form.classList.add('was-validated')
-    }, false)
-  })
-})()
+window.addEventListener("load", function() {
+  const cform = document.getElementById('contact-form');
+  cform.addEventListener("submit", function(e) {
+    e.preventDefault();
+    const data = new FormData(cform);
+    const action = e.target.action;
+    fetch(action, {
+      method: 'POST',
+      body: data,
+    })
+    .then(() => {
+      alert("Success!");
+    })
+  });
+});
