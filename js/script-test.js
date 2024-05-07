5f1340f99920cda8d3be79724ec3887f9f7df86d
kpao2020 <xoakuma@gmail.com>
2024-05-06 17:11:07 -0700

    add msg below submit

--- js/script-test.js
+++ js/script-test.js
@@ -36,31 +36,58 @@ bioCloseBtn.addEventListener("click", () => {
     bioCloseBtn.classList.toggle("d-none");
 });
 
-(() => {
-    'use strict'
+// (() => {
+//     'use strict'
   
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    const forms = document.querySelectorAll('.needs-validation')
+//     // Fetch all the forms we want to apply custom Bootstrap validation styles to
+//     const forms = document.querySelectorAll('.needs-validation')
   
-    // Loop over them and prevent submission
-    Array.from(forms).forEach(form => {
-      form.addEventListener('submit', event => {
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
-        } 
-        else {
-          alert("Thank you for submitting your contact information successfully.");
-        }
+//     // Loop over them and prevent submission
+//     Array.from(forms).forEach(form => {
+//       form.addEventListener('submit', event => {
+//         if (!form.checkValidity()) {
+//           event.preventDefault()
+//           event.stopPropagation()
+//         } 
+//         else {
+//           alert("Thank you for submitting your contact information successfully.");
+//           if (form.classList.contains('was-validated')){
+//             form.preventDefault();
+//     const data = new FormData(form);
+//     const action = form.target.action;
+//     fetch(action, {
+//       method: 'POST',
+//       body: data,
+//     })
+//     .then(() => {
+//       alert("Success!");
+//     })
+//           }
+          
+//         }
   
-        form.classList.add('was-validated')
-      }, false)
-    })
-  })()
+//         form.classList.add('was-validated')
+//       }, false)
+//     })
+//   })()
+
+// function sendGoogle(e){
+//   e.preventDefault();
+//   const cform = document.getElementById('contact-form');
+//   const data = new FormData(cform);
+//   const action = e.target.action;
+//   fetch(action, {
+//       method: 'POST',
+//       body: data,
+//     }).then(() => {
+//       alert("Success!");
+//     })
+//   return false;
+// }
 
 window.addEventListener("load", function() {
   const cform = document.getElementById('contact-form');
-  cform.addEventListener("submit", function(e) {
+  cform.addEventListener("click", function(e) {
     e.preventDefault();
     const data = new FormData(cform);
     const action = e.target.action;
@@ -70,6 +97,7 @@ window.addEventListener("load", function() {
     })
     .then(() => {
       alert("Success!");
-    })
+    });
+    return false;
   });
 });
